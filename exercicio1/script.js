b1daedcf82d95104975d2ead4723758bc22897b2
AnaliceMarques <analicemarquessl@gmail.com>
2022-11-22 12:06:10 -0300

    Resolução do Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+//Exercício 1
+
+let numPar = Number(prompt("Informe um número par:"))
+
+let resto
+resto = numPar % 2
+
+console.log("O número", numPar, "dividido por 2 tem resto", resto)
+//Quando eu começo o console.log com a variável ela coloca aspas no texto que fica entre as váriaveis. (VERIFICAR)
+
+//Todo número par quando dividido por 2 dá resto 0.
+
+//Todo número ímpar quando dividido por 2 dá resto 1.
